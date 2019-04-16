04d47218ea08c041e9b812df5aeafc6958e25a9f
Claire Fahy <zeebear21@gmail.com>
2019-04-16 16:23:22 -0300

    postgresql database working?

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_16_012147) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
